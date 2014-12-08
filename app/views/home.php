23ceb587d01b358720e7b509a0f76bad1dd075ad
Ryan O'Hara <ryan@ryanohara.co>
2014-12-08 10:35:58 -0500

    Creating some necessary files

---
+++ app/views/home.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ryan W. O'Hara's To-Do</title>
+	<style type="text/css">
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+		@import url(/css/style.css);
+	</style>
+	<script src="/js/angular.min.js"></script>
+	<script src="/js/script.js"></script>
+</head>
+<body>
+	<div class="container">
+		
+	</div>
+</body>
+</html>
